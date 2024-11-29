6084fc8ef145f87f1a8862f10de39d4800a6d6b3
fishheaddd <Emofish56@gmail.com>
2024-11-29 18:01:31 +0800

    Add files via upload

---
+++ javascript/javascript_finals.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    const container = document.querySelector('.container');
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const message = document.getElementById('login-message');
+
+        const storedUsername = localStorage.getItem('username');
+        const storedPassword = localStorage.getItem('password');
+
+        if (username === storedUsername && password === storedPassword) {
+            localStorage.setItem('loggedIn', 'true');
+            window.location.href = 'home.html';
+        } else {
+            message.textContent = 'INVALID. PLEASE TRY AGAIN';
+            message.style.color = 'rgb(255, 255, 255)';
+            message.style.background = '#fc0d7d';
+        }
+    };
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const username = document.getElementById('registerusername').value;
+        const password = document.getElementById('registerpassword').value;
+        const confirmPassword = document.getElementById('registerconfirm').value;
+        
+        const message = document.getElementById('registermessage');
+
+        if (password !== confirmPassword) {
+            message.textContent = 'PLEASE INPUT AGAIN.';
+            message.style.color = 'rgb(255, 255, 255)';
+            message.style.background = '#fc0d7d';
+            return;
+        }
+
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+        message.textContent = 'SUCCESSFUL';
+        message.style.color = '#d1b500';
+        message.style.background = '#002324';
+        message.style.fontWeight = '700'; 
+
+        setTimeout(() => {
+            window.location.href = 'index.html';
+        }, 2000);
+    };
+
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', handleLogin);
+    }
+
+    if (registerForm) {
+        registerForm.addEventListener('submit', handleRegister);
+    }
+
+});
